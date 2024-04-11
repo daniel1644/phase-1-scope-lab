bc3e979ff08c06552a27eb2144b84fc2b4f84390
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-04-11 12:43:53 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+customerName = 'bob';
+
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+
+
+var customerName = 'bob';
+var bestCustomer;
+function setBestCustomer() {
+    if (customerName !== 'bob') {
+      bestCustomer = 'not bob';
+    }
+    return bestCustomer;
+  }
+  setBestCustomer();
+
+
+
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+  }
+
+
+
+  const leastFavoriteCustomer = 'some value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another value';
+  } 
+
+
+
